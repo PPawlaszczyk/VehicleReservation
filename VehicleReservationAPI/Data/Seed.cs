07de2e8de361bbda5a290849b28e3007a6ee6032
Paweł <pawel.pawlaszczyk@gmail.com>
2025-01-30 02:25:22 +0100

    [Seed] Add seed for vehicles

--- VehicleReservationAPI/Data/Seed.cs
+++ VehicleReservationAPI/Data/Seed.cs
@@ -1,22 +1,26 @@
 ﻿using System.Text.Json;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using VehicleReservationAPI.CQRS.Vehicles.Commands;
 using VehicleReservationAPI.Entities;
+using VehicleReservationAPI.Interfaces;
 
 namespace VehicleReservationAPI.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+        public static async Task SeedUsers(IUnitOfWork unitOfWork, UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
 
             if (await userManager.Users.AnyAsync()) return;
 
             var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+            var vehicleData = await File.ReadAllTextAsync("Data/VehicleSeed.json");
 
             var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData, options);
+            var vehicles = JsonSerializer.Deserialize<List<CreateVehicleCommand>>(vehicleData, options);
 
             if (users == null) return;
 
@@ -35,6 +39,11 @@ namespace VehicleReservationAPI.Data
                 user.UserName = user.UserName!.ToLower(); ;
                 await userManager.CreateAsync(user, "Pa$$w0rd");
                 await userManager.AddToRoleAsync(user, "Member");
+            }
+
+            foreach (var vehicle in vehicles)
+            {
+                unitOfWork.VehiclesRepository.AddVehicle(vehicle);
             }
 
             var admin = new AppUser
