9231c3152c29dfb27a35d9c6a02b0f167bdf4032
Paweł <pawel.pawlaszczyk@gmail.com>
2025-02-03 05:02:29 +0100

    [Refactoring] Change name order

--- VehicleReservationAPI/Entities/Reservation.cs
+++ VehicleReservationAPI/Entities/Reservation.cs
@@ -8,9 +8,9 @@ namespace VehicleReservationAPI.Entities
         public Vehicle? Vehicle { get; set; }
         public required Guid VehicleId { get; set; }
         public required Guid AppUserId { get; set; }
-        public required DateOnly EndDate { get; set; }
-        public required DateOnly StartDate { get; set; }
         public required Status Status { get; set; }
+        public required DateOnly StartDate { get; set; }
+        public required DateOnly EndDate { get; set; }
         public required DateTime Created { get; set; }
     }
 }
